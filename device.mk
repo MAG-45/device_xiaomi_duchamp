dce84a711ec6dab5911457cecb2f3f84868406bf
MAG <raphvmag@ik.me>
2025-02-22 19:52:33 +0000

    duchamp: overlay-evolution: Add Overlays Settings/Updater
    - Add UpdaterOverlay => OTA on MAG-45 repo
    - Add SettingsEvoOverlay => Credit MAG on Settings app
    - Change device.mk to integrate overlay-evolution
    
    duchamp: lineage_duchamp.mk
    
    - Update derp_duchamp to lineage_duchamp

--- device.mk
+++ device.mk
@@ -202,6 +202,10 @@ PRODUCT_PACKAGES += \
    LineageSDKOverlayDuchamp \
    LineageSettingsOverlayDuchamp
 
+PRODUCT_PACKAGES += \
+    SettingsEvoDuchamp \
+    UpdaterEvoDuchamp
+
 # Parts
 PRODUCT_PACKAGES += \
     XiaomiParts
