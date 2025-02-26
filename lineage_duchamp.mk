f80cc3d16731000b7d74d4d1cb8476fa5654667d
MAG <raphvmag@ik.me>
2025-02-26 18:05:45 +0000

    duchamp: makefile: rename derp to lineage

--- lineage_duchamp.mk
+++ lineage_duchamp.mk
@@ -11,13 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/duchamp/device.mk)
 
-# Inherit some common DerpfestOS stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Derpfest
-EXTRA_UDFPS_ICONS := true
-
-PRODUCT_NAME := derp_duchamp
+PRODUCT_NAME := lineage_duchamp
 PRODUCT_DEVICE := duchamp
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -31,3 +28,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=POCO/duchamp_global/duchamp:14/UP1A.230905.011/OS2.0.1.0.VNLMIXM:user/release-keys \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
+TARGET_HAS_UDFPS := true
