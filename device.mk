3ccaad3e045ac4dd7749e0c306f0c379901f2a47
MAG <raphvmag@ik.me>
2025-03-04 18:40:35 +0000

    duchamp: dolby: add dolby support

--- device.mk
+++ device.mk
@@ -411,3 +411,6 @@ PRODUCT_PACKAGES += \
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/duchamp/duchamp-vendor.mk)
+
+# Dolby 
+$(call inherit-product, hardware/dolby/dolby.mk)
