99dbb387a3716096427c4ef2b6c48b9b4a924355
MAG <raphvmag@ik.me>
2025-03-26 11:43:46 +0000

    duchamp: add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,25 @@
+# 1. Apply Git Changes (Source Modification)
+echo "Applying source modifications..."
+
+# UDFPS Patch for frameworks/base
+echo "Patch FP"
+cd frameworks/base
+wget https://raw.githubusercontent.com/MAG-45/duchamp_patch/refs/heads/vic_evo/fp_evo.patch
+git am < fp_evo.patch
+cd ../../
+
+# Hardware Compatibility Fixes
+echo "Fetching and applying hardware compatibility fixes for hardware/lineage/compat..."
+cd hardware/lineage/compat
+git fetch https://github.com/xiaomi-mt6897-duchamp/android_hardware_lineage_compat
+git cherry-pick 9a046ea7e5899adc38ab04fe24eb34859fe4d779
+cd ../../../
+
+# Add WPA3 fix patch
+echo "Patching WPA3 to work on duchamp device..."
+cd external/wpa_supplicant_8
+git fetch https://github.com/xiaomi-mt6897-duchamp/android_external_wpa_supplicant_8
+git cherry-pick cc88629c6c5c2c2353bf87efef9b5c9c5bf32bee
+cd ../../
+
+echo "All operations completed successfully!"
