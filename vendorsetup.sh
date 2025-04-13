cfe6bf2a8f7339b25235fa5060a10a30dc58955c
MAG <raphvmag@ik.me>
2025-04-13 11:45:58 +0000

    duchamp: update vendorsetup for WPA2/WPA3 fix

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,11 +15,21 @@ git fetch https://github.com/xiaomi-mt6897-duchamp/android_hardware_lineage_comp
 git cherry-pick 9a046ea7e5899adc38ab04fe24eb34859fe4d779
 cd ../../../
 
+
+# OLD WAY
 # Add WPA3 fix patch
-echo "Patching WPA3 to work on duchamp device..."
-cd external/wpa_supplicant_8
-git fetch https://github.com/xiaomi-mt6897-duchamp/android_external_wpa_supplicant_8
-git cherry-pick cc88629c6c5c2c2353bf87efef9b5c9c5bf32bee
-cd ../../
+#echo "Patching WPA3 to work on duchamp device..."
+#cd external/wpa_supplicant_8
+#git fetch https://github.com/xiaomi-mt6897-duchamp/android_external_wpa_supplicant_8
+#git cherry-pick cc88629c6c5c2c2353bf87efef9b5c9c5bf32bee
+#cd ../../
+
+# NEW WAY
+# Add WPA2/WPA3 fix
+echo "Patching WPA2/3 to work on duchamp device"
+cd external
+rm -rf wpa_supplicant_8
+git clone https://github.com/snapboss/test_external_wpa_supplicant_8.git wpa_supplicant_8 --depth=1
+cd ../
 
 echo "All operations completed successfully!"
